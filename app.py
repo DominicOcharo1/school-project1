829583b07747f1a8262e1c4c872405c553b18a81
DominicOcharo1 <149952803+DominicOcharo1@users.noreply.github.com>
2023-11-06 16:55:58 +0300

    Update app.py
    centered the title and authors

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ st.set_page_config(
 )
 
 # Main page heading
-with st.container():
-    st.title("DL POWERED PESTS AND DISEASE DETECTION IN MAIZE")
-    st.markdown("<h1 style='text-align: center; color: grey;'>Big headline</h1>", unsafe_allow_html=True)
+with st.container(): # st.title("DL POWERED PESTS AND DISEASE DETECTION IN MAIZE")
+    st.markdown("<h1 style='text-align: center; color: blue;'>DL POWERED PESTS AND DISEASE DETECTION IN MAIZE</h1>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: black;'>By Dominic Ocharo and Shadrack Onjiri</h2>", unsafe_allow_html=True)
     
 with st.container():
     st.write("---")
