94e4fe00c1ad0e0907b5c4143fc80b9f9207e0ed
DominicOcharo1 <149952803+DominicOcharo1@users.noreply.github.com>
2023-11-16 12:21:46 +0300

    Update app.py
    changed sidebar look

--- app.py
+++ app.py
@@ -43,8 +43,6 @@ with st.container():
     st.markdown('Then click the Detect Objects button and check the result.')
 
 # Sidebar
-st.sidebar.header("ML Model Config")
-
 # Model Options
 model_type = st.sidebar.radio(
     "Select Task", ['Detection', 'Detection1'])
@@ -52,66 +50,71 @@ model_type = st.sidebar.radio(
 confidence = float(st.sidebar.slider(
     "Select Model Confidence", 20, 10, 1)) / 20
 
-# Selecting Detection Or Segmentation
-if model_type == 'Detection':
-    model_path = Path(settings.DETECTION_MODEL)
-elif model_type == 'Detection1':
-    model_path = Path(settings.SEGMENTATION_MODEL)
 
-# Load Pre-trained ML Model
-try:
-    model = helper.load_model(model_path)
-except Exception as ex:
-    st.error(f"Unable to load model. Check the specified path: {model_path}")
-    st.error(ex)
-
-st.sidebar.header("Image/Video Config")
-source_radio = st.sidebar.radio(
-    "Select Source", settings.SOURCES_LIST)
-
-source_img = None
-# If image is selected
-if source_radio == settings.IMAGE:
-    source_img = st.sidebar.file_uploader(
-        "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        try:
-            if source_img:
-                uploaded_image = PIL.Image.open(source_img)
-                st.image(source_img, caption="Uploaded Image",
-                         use_column_width=True)
-        except Exception as ex:
-            st.error("Error occurred while opening the image.")
-            st.error(ex)
-
-    with col2:        
-            if st.sidebar.button('Detect Objects'):
-                res = model.predict(uploaded_image,
-                                    conf=confidence
-                                    )
-                boxes = res[0].boxes
-                res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption="Detected Image",
-                         use_column_width=True)
-                try:
-                    with st.expander("Detection Results"):
-                        for box in boxes:
-                            st.write(box.data)
-                except Exception as ex:
-                    # st.write(ex)
-                    st.write("No image is uploaded yet!")
-
-elif source_radio == settings.VIDEO:
-    helper.play_stored_video(confidence, model)
-
-elif source_radio == settings.WEBCAM:
-    helper.play_webcam(confidence, model)
-
-elif source_radio == settings.YOUTUBE:
-    helper.play_youtube_video(confidence, model)
-
-else:
-    st.error("Please select a valid source type!")
+with st.container():
+    st.write("---")
+    st.header("ML Model Config")
+    
+    # Selecting Detection1 Or Detection2
+    if model_type == 'Detection':
+        model_path = Path(settings.DETECTION_MODEL)
+    elif model_type == 'Detection1':
+        model_path = Path(settings.SEGMENTATION_MODEL)
+    
+    # Load Pre-trained ML Model
+    try:
+        model = helper.load_model(model_path)
+    except Exception as ex:
+        st.error(f"Unable to load model. Check the specified path: {model_path}")
+        st.error(ex)
+    
+    st.header("Image/Video Config")
+    source_radio = st.radio(
+        "Select Source", settings.SOURCES_LIST)
+    
+    source_img = None
+    # If image is selected
+    if source_radio == settings.IMAGE:
+        source_img = st.file_uploader(
+            "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+    
+        col1, col2 = st.columns(2)
+    
+        with col1:
+            try:
+                if source_img:
+                    uploaded_image = PIL.Image.open(source_img)
+                    st.image(source_img, caption="Uploaded Image",
+                             use_column_width=True)
+            except Exception as ex:
+                st.error("Error occurred while opening the image.")
+                st.error(ex)
+    
+        with col2:        
+                if st.button('Detect Objects'):
+                    res = model.predict(uploaded_image,
+                                        conf=confidence
+                                        )
+                    boxes = res[0].boxes
+                    res_plotted = res[0].plot()[:, :, ::-1]
+                    st.image(res_plotted, caption="Detected Image",
+                             use_column_width=True)
+                    try:
+                        with st.expander("Detection Results"):
+                            for box in boxes:
+                                st.write(box.data)
+                    except Exception as ex:
+                        # st.write(ex)
+                        st.write("No image is uploaded yet!")
+    
+    elif source_radio == settings.VIDEO:
+        helper.play_stored_video(confidence, model)
+    
+    elif source_radio == settings.WEBCAM:
+        helper.play_webcam(confidence, model)
+    
+    elif source_radio == settings.YOUTUBE:
+        helper.play_youtube_video(confidence, model)
+    
+    else:
+        st.error("Please select a valid source type!")
